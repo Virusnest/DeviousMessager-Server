c23b39d65f55853b6c836b5787614b7475e48065
Virusnest <kai.maloney.au@gmail.com>
2022-10-27 09:23:59 +1100

    add comments and tweek css

--- index.js
+++ index.js
@@ -9,27 +9,29 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 app.get('')
+//open client webserver
 
 io.on('connection', (socket) => {
   var name
   console.log('a user connected');
-  socket.on('userdata',(uname)=>{
-    name=uname
-    io.emit('sysmessage',name+" Joined The Lobby","green")
-  })
-  //Get user data
-
   socket.on('chat message', (msg) => {
     socket.broadcast.emit('chat message', msg,name);
     //console.log(name,":",msg)
   });
-  //manage chat messages
+  //
   socket.on('disconnect', () => {
     console.log('user disconnected');
-    io.emit('sysmessage',name+" Disconected","red")
+    io.emit('sysmessage',name+" Disconected", "#f38ba8")
   });
+  socket.on('userdata',(uname)=>{
+    name=uname
+    //get user data
+    io.emit('sysmessage',name+" Joined The Lobby","#a6e3a1")
+  })
+  //connect and disconect messages
 });
 
 server.listen(3000, () => {
   console.log('listening on *:3000');
 });
+//open server
